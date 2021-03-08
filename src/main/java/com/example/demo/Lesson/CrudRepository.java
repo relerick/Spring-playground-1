ed1b76528c2f88a6574ed6939487cb44d7d6fe30
robertscalzo <77977784+robertscalzo@users.noreply.github.com>
2021-03-08 14:30:32 -0600

    Fixing SRC

--- src/main/java/com/example/demo/Lesson/CrudRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.Lesson;
-
-
-
-public interface CrudRepository<T, T1> {
-}
--- src/main/java/com/example/demo/Lesson/LessonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Lesson;
-
-import org.springframework.data.repository.CrudRepository;
-public interface LessonRepository extends CrudRepository<Lesson, Long> {
-
-
-}
